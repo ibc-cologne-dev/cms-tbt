75289e6029db5d7e1fa4fb6c3b1e8cdef0e36984
John <jonnydeveloper22@gmail.com>
2021-11-01 13:31:45 +0100

    Added nanoid to yarn and packages. Added IBC-Logo.#

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,15 @@ import "typeface-space-mono";
 import { tbtCollection } from "./collections/tbt";
 import { resourceTypeCollection } from "./collections/resourceType";
 
+ const firebaseConfig = {
+   apiKey: "AIzaSyCQ6faWwixqUb_jbDJXk8EEcRJUYNxIOCg",
+   authDomain: "cms-tbt.firebaseapp.com",
+   projectId: "cms-tbt",
+   storageBucket: "cms-tbt.appspot.com",
+   messagingSenderId: "83044330620",
+   appId: "1:83044330620:web:8c60ff0cc7aff2a0522df9",
+  };
+
 export default function App() {
   const navigation: NavigationBuilder = async () => {
     return {
@@ -27,6 +36,7 @@ export default function App() {
       authentication={myAuthenticator}
       navigation={navigation}
       firebaseConfig={firebaseConfig}
+      logo={"IBC-Logo-Login.png"}
     />
   );
 }
