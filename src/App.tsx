46ff714a4c2c48aa43b641e84077ed4b0a78d8b1
Carlos Alberto de Araujo Barreto <araujobarret@gmail.com>
2021-11-02 10:19:59 +0100

    Improve setup from Firebase (#7)
    * chore(setup): improve firebase setup
    
    * setup(init): improve setup on cms

--- src/App.tsx
+++ src/App.tsx
@@ -8,15 +8,7 @@ import "typeface-space-mono";
 
 import { tbtCollection } from "./collections/tbt";
 import { resourceTypeCollection } from "./collections/resourceType";
-
- const firebaseConfig = {
-   apiKey: "AIzaSyCQ6faWwixqUb_jbDJXk8EEcRJUYNxIOCg",
-   authDomain: "cms-tbt.firebaseapp.com",
-   projectId: "cms-tbt",
-   storageBucket: "cms-tbt.appspot.com",
-   messagingSenderId: "83044330620",
-   appId: "1:83044330620:web:8c60ff0cc7aff2a0522df9",
-  };
+import { firebaseConfig } from "./firebase";
 
 export default function App() {
   const navigation: NavigationBuilder = async () => {
---
+++ src/firebase.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCQ6faWwixqUb_jbDJXk8EEcRJUYNxIOCg",
+  authDomain: "cms-tbt.firebaseapp.com",
+  projectId: "cms-tbt",
+  storageBucket: "cms-tbt.appspot.com",
+  messagingSenderId: "83044330620",
+  appId: "1:83044330620:web:8c60ff0cc7aff2a0522df9",
+};
